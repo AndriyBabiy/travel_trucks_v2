c0223c8064f2e41bc4db7a38f8b806af9e9cce18
Andriy Babiy <andriybabiy97@gmail.com>
2024-10-29 00:52:31 +0000

    fixed location of product call

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,34 +1,34 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import ProductCard from "./ProductCard/ProductCard";
 import css from "./ProductList.module.css";
-import { selectProducts } from "../../redux/selectors";
-import { useEffect, useState } from "react";
+// import { selectProducts } from "../../redux/selectors";
+// import { useEffect, useState } from "react";
 import NavButton from "../Button/NavButton/NavButton";
 
-function ProductList() {
-  const products = useSelector(selectProducts);
+function ProductList({ products, loadMore }) {
+  // const products = useSelector(selectProducts);
 
-  const [visibleItemCount, setVisibleItemCount] = useState(4);
-  const [displayedData, setDisplayedData] = useState(
-    products?.slice(0, visibleItemCount)
-  );
+  // const [visibleItemCount, setVisibleItemCount] = useState(4);
+  // const [displayedData, setDisplayedData] = useState(
+  //   products?.slice(0, visibleItemCount)
+  // );
 
-  useEffect(() => {
-    setDisplayedData(products?.slice(0, visibleItemCount));
-  }, [visibleItemCount, products]);
+  // useEffect(() => {
+  //   setDisplayedData(products?.slice(0, visibleItemCount));
+  // }, [visibleItemCount, products]);
 
-  const handleLoadMore = () => {
-    setVisibleItemCount((prevCount) => prevCount + 4);
+  // const handleLoadMore = () => {
+  //   setVisibleItemCount((prevCount) => prevCount + 4);
 
-    setDisplayedData(products.slice(0, visibleItemCount + 4));
-  };
+  //   setDisplayedData(products.slice(0, visibleItemCount + 4));
+  // };
 
   return (
     <div className={css.container}>
-      {displayedData?.map((product) => (
+      {products?.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
-      <NavButton onClick={handleLoadMore} variant={"loadMore"}>
+      <NavButton onClick={loadMore} variant={"loadMore"}>
         Load more
       </NavButton>
     </div>
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -2,12 +2,15 @@ import Filters from "../../components/Sidebar/Sidebar";
 import ProductList from "../../components/ProductList/ProductList";
 import css from "./CatalogPage.module.css";
 import { fetchProducts } from "../../redux/operations";
-import { useDispatch } from "react-redux";
-import { useEffect, useMemo, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useEffect, useMemo, useState } from "react";
+import { selectProducts } from "../../redux/selectors";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const CatalogPage = () => {
   const dispatch = useDispatch();
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
@@ -15,28 +18,55 @@ const CatalogPage = () => {
   }, [dispatch]);
   // const productsList = dispatch(useSelector(selectProducts));
 
-  const productList = useMemo(() => {
-    const fetchProducts = async () => {
-      try {
-        setIsLoading(true);
-        setProducts(fetchProducts());
-      } catch (e) {
-        console.log(e);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  // const productList = useMemo(() => {
+  //   const fetchProducts = async () => {
+  //     try {
+  //       setIsLoading(true);
+  //       setProducts(fetchProducts());
+  //     } catch (e) {
+  //       console.log(e);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   };
 
-    fetchProducts();
-  }, []);
+  //   fetchProducts();
+  // }, []);
 
-  console.log("productList", productList);
-  console.log("products", products);
+  const products = useSelector(selectProducts);
+
+  const [visibleItemCount, setVisibleItemCount] = useState(4);
+  const [displayedData, setDisplayedData] = useState(
+    products?.slice(0, visibleItemCount)
+  );
+
+  useEffect(() => {
+    try {
+      setIsLoading(true);
+      setDisplayedData(products?.slice(0, visibleItemCount));
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [visibleItemCount, products]);
+
+  const handleLoadMore = () => {
+    setVisibleItemCount((prevCount) => prevCount + 4);
+
+    setDisplayedData(products.slice(0, visibleItemCount + 4));
+  };
+
+  console.log("products", displayedData);
 
   return (
     <div className={css.container}>
       <Filters />
-      {isLoading ? <h2>Loading</h2> : <ProductList />}
+      {isLoading ? (
+        <h2>Loading</h2>
+      ) : (
+        <ProductList products={displayedData} loadMore={handleLoadMore} />
+      )}
     </div>
   );
 };
